5d5bea965fad8e682d80d7d4a4606b3a4eee9db7
mgwendolyn <mgwendolyn.moscardini@gmail.com>
2015-02-23 20:16:02 -0500

    Update app.js

--- client/js/app.js
+++ client/js/app.js
@@ -13,32 +13,21 @@ function getSessionDetails(sessionId, success, error) {
   force.query(soql, success, error);
 }
 
-function showSessionDetails(sessionId) {
-
-    getSessionDetails(sessionId,
+function showSessionList() {
+    getSessionList(
         function (data) {
-            var session = data.records[0],
+            var sessions = data.records,
+                html = '';
+            for (var i=0; i<sessions.length; i++) {
+                html += '<li class="table-view-cell"><a href="#sessions/'+ sessions[i].Id +'">' + sessions[i].Name + ' - ' + sessions[i].Level__c + ' - ' + sessions[i].Session_Date__c + '</a></li>';
+            }
             html =
                 '<div class="page">' +
                 '<header class="bar bar-nav">' +
-                '<a class="btn btn-link btn-nav pull-left" href="#"><span class="icon icon-left-nav"></span>Back</a>' +
-            '<h1 class="title">Sessions</h1>' +
+                    '<h1 class="title">Sessions</h1>' +
                 '</header>' +
                 '<div class="content">' +
-                    '<div class="card">' +
-                        '<ul class="table-view">' +
-                            '<li class="table-view-cell">' +
-                                '<h4>' + session.Session__r.Name + '</h4>' +
-                                '<p>' + (session.Session__r.Session_Date__c || 'No time yet')+ '</p>' +
-                            '</li>' +
-                            '<li class="table-view-cell">Speaker: ' +
-                                session.Speaker__r.First_Name__c +
-                            '</li>' +
-                            '<li class="table-view-cell">' +
-                                (session.Session__r.Description__c || 'No description yet') +
-                            '</li>' +
-                        '</ul>' +
-                    '</div>' +
+                    '<ul class="table-view session-list">' + html + '</ul>' +
                 '</div>' +
                 '</div>';
             slider.slidePage($(html));
